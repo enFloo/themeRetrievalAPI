c496765d15e7e50a3001941ce336a88f419b4150
Florian Houenou <88054514+enFloo@users.noreply.github.com>
2023-05-18 06:37:36 -0500

    fixed error I was getting. Now error free

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -4,19 +4,19 @@ import React from 'react';
 export default function Form(){
     return(
         <form>
-            <div class="from-group">
-                <input type="text" class="form-control" name="name" placeholder="Product Name"/>
+            <div >
+                <input type="text"  name="name" placeholder="Product Name"/>
             </div>
-            <div class="from-group">
-                <input type="text" class="form-control" name="thumbnailURL" placeholder="ThumbnailURL"/>
+            <div >
+                <input type="text"  name="thumbnailURL" placeholder="ThumbnailURL"/>
             </div>
-            <div class="from-group">
-                <input type="text" class="form-control" name="sourceURL" placeholder="SourceURL"/>
+            <div >
+                <input type="text"  name="sourceURL" placeholder="SourceURL"/>
             </div>
-            <div class="from-group">
-                <input type="text" class="form-control" name="category" placeholder="Category"/>
+            <div >
+                <input type="text"  name="category" placeholder="Category"/>
             </div>
-            <button class="btn btn-primary btn-block">Save</button>
+            <button >Save</button>
         </form>
     )
 }
